1760de5dee3e7022c21c8d26a35ff4c68fdcd48f
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2024-08-04 21:12:57 -0700

    update

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Scripts then will unlock list of lists"""
+
+def canUnlockAll(boxes):
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
