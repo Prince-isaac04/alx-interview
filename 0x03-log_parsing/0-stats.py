3f917f806c8fc3904a5d3fc5588874269cd865f6
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2024-08-15 18:35:38 -0700

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -37,8 +37,7 @@ try:
                 sts[stlist[-2]] += 1
         except IndexError:
             pass
-    
-    printsts(sts, size)
+        printsts(sts, size)
 
 except KeyboardInterrupt:
     printsts(sts, size)
