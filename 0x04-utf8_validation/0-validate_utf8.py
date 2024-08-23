c5749fb81339b84d84bd2408025e4d1b7f147521
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2024-08-22 21:19:30 -0700

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""UTF-8 Validation module"""
+"""UTF-8 Validation"""
 
 
-def valid_utf8(data):
-    """
-    Determines if a given data set represents
-    a valid UTF-8 encoding.
-
-    :param data: List of integers representing bytes
-    :return: True if data is valid UTF-8, False otherwise
+def validUTF8(data):
+    """Determines if a given data set
+    represents a valid utf-8 encoding
     """
     number_bytes = 0
 
@@ -16,9 +12,11 @@ def valid_utf8(data):
     mask_2 = 1 << 6
 
     for i in data:
+
         mask_byte = 1 << 7
 
         if number_bytes == 0:
+
             while mask_byte & i:
                 number_bytes += 1
                 mask_byte = mask_byte >> 1
@@ -28,11 +26,15 @@ def valid_utf8(data):
 
             if number_bytes == 1 or number_bytes > 4:
                 return False
+
         else:
             if not (i & mask_1 and not (i & mask_2)):
                 return False
 
         number_bytes -= 1
 
-    return number_bytes == 0
+    if number_bytes == 0:
+        return True
+
+    return False
 
